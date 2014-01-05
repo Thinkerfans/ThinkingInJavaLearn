9a4ca9f591165a125d7a690c116c0bbc2f07052f
Thinkerfans <2656079093@qq.com>
2014-01-05 22:18:23 +0800

    no change

--- src/com/java/chapter10/Parcel1.java
+++ src/com/java/chapter10/Parcel1.java
@@ -1,45 +1,50 @@
 package com.java.chapter10;
 
 public class Parcel1 {
-	
-	class Contents{
-		private int i =1;
-		public int value(){
+
+	class Contents {
+		private int i = 1;
+
+		public int value() {
 			return i;
 		}
-		public Contents(){			
+
+		public Contents() {
 		}
 	}
-	
-	class Destination{
+
+	class Destination {
 		private String label;
-		public Destination(String s){
+
+		public Destination(String s) {
 			this.label = s;
 		}
-		public String readLabel () {
+
+		public String readLabel() {
 			return label;
 		}
 	}
 
-	public void userInnerClass(){
+	public void userInnerClass() {
 		Contents c = new Contents();
 		Destination d = new Destination("test");
 		System.out.println(d.label);
-		System.out.println(d.readLabel());	
+		System.out.println(d.readLabel());
 	}
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Parcel1 p = new Parcel1();
-		p.userInnerClass();	
-		
+		p.userInnerClass();
+
 		Contents c = p.new Contents();
 		Destination d = p.new Destination("test2");
 		System.out.println(d.label);
-		System.out.println(d.readLabel());	
+		System.out.println(d.readLabel());
 	}
 
 }
