0f66adaa321b56aa1d6f761f6743e3673de6447a
Thinkerfans <2656079093@qq.com>
2014-02-12 13:40:27 +0800

    static key  use after import

---
+++ src/com/thinker/log/Print.java
@@ -0,0 +1,14 @@
+package com.thinker.log;
+
+public class Print {
+	
+	public static String tag = "use static method or field direct,not by class.method or class.field";
+	public static void print(Object obj){
+		System.out.println(obj);
+	}
+	
+	public void prinnt(){
+		
+	}
+
+}
---
+++ src/com/thinker/log/PrintTest.java
@@ -0,0 +1,17 @@
+package com.thinker.log;
+
+import static com.thinker.log.Print.*;
+// 注意此import static的特效，相当于c里面的include头文件，引入后
+// 直接使用其里面的静态方法和属性。
+// 详细说明：http://docs.oracle.com/javase/1.5.0/docs/guide/language/static-import.html
+
+
+public class PrintTest {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		print(tag);
+	}
+}
