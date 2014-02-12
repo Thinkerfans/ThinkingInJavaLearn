14430032a54adf826848716c50d43d47deaa6335
U-PCOS-05161118\Administrator <Administrator@PCOS-05161118.(none)>
2014-02-12 13:37:14 +0800

    public key use in class

---
+++ src/com/java/chapter5/KeyPublicUseInClass.java
@@ -0,0 +1,13 @@
+package com.java.chapter5;
+
+/**
+ * if no public class in java file ,the class can be different to java file name
+ *1、 如何java文件里面没有public修饰的类存在，那么类名可以与java文件名不一致。
+ * 2、若有public类，那么它修饰的类名必须与java文件名一致，且最多只能有public类。
+ */
+
+class One{	
+}
+
+class Two{	
+}
